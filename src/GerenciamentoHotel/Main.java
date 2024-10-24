085437b8eac423ee16be70a43fee7c874265ab0f
anedfranca <anedourado@icloud.com>
2024-10-24 18:35:48 -0300

    finalizando menu inicial

--- src/GerenciamentoHotel/Main.java
+++ src/GerenciamentoHotel/Main.java
@@ -1,15 +1,45 @@
 package GerenciamentoHotel;
 
-import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-
 		
+		Scanner sc = new Scanner(System.in);
+		int op1, op2;
+		
+		while (true) {
+			System.out.println("\n1. Quartos");
+			System.out.println("2. Hospedes");
+			System.out.println("3. Reservas");
+			System.out.println("4. Funcionarios");
+			System.out.println("5. Check-in/Check-out");
+			System.out.println("6. Sair\n");
+			
+			System.out.print("Digite a opcao desejada: ");
+			op1 = sc.nextInt();
+			
+			switch (op1) {
+				case 1:
+					//
+				break;
+				case 2:
+					//
+				break;
+				case 3:
+					//
+				break;
+				case 4:
+					//
+				break;
+				case 5:
+					//
+				break;
+				case 6:
+					//
+				break;
+			}
+		}
 	}
-
-	
-
 }
