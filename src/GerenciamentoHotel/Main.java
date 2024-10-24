48f31d1a3edd7e8606e6f9d6492b60a807e012c1
anedfranca <anedourado@icloud.com>
2024-10-24 18:17:36 -0300

    initting menu options

--- src/GerenciamentoHotel/Main.java
+++ src/GerenciamentoHotel/Main.java
@@ -6,20 +6,8 @@ public class Main {
 
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
+
 		
-			
-		Pessoa prefeito = new Pessoa();
-		
-		prefeito.setNome("Beto Lelis");
-		
-		System.out.println(prefeito.getNome());
-		
-		Hospede h1 = new Hospede();
-		Hospede h2 = new Hospede("Ane dourada", "7878787", new Date(10, 10, 2000), "Irece BA", "83 99999-8765");
-		
-		
-		
-		System.out.println(h2);
 	}
 
 	
