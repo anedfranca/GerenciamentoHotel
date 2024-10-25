9e9742e8cafbcdafc513e617ca1d9db587901445
anedfranca <anedourado@icloud.com>
2024-10-25 17:30:53 -0300

    Iniciando HUD funcionarios

--- src/GerenciamentoHotel/Funcionario.java
+++ src/GerenciamentoHotel/Funcionario.java
@@ -4,7 +4,57 @@ public class Funcionario extends Pessoa {
 	private String cargo;
 	private Double salario;
 	private String turnoDeTrabalho;
+	private Integer qtdeHorasTrabalhadas;
+	private Double valorHoras;
 	
+	public Funcionario() {
+		super();
+	}
 	
+	public Funcionario(String nome, String cpf, String cargo, String tdt) {
+		super(nome, cpf);
+		this.setCargo(cargo);
+		this.setTurnoDeTrabalho(tdt);
+	}
+
+	public Integer getQtdeHorasTrabalhadas() {
+		return qtdeHorasTrabalhadas;
+	}
+
+	public void setQtdeHorasTrabalhadas(Integer qtdeHorasTrabalhadas) {
+		this.qtdeHorasTrabalhadas = qtdeHorasTrabalhadas;
+	}
+
+	public Double getValorHoras() {
+		return valorHoras;
+	}
+
+	public void setValorHoras(Double valorHoras) {
+		this.valorHoras = valorHoras;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public String getTurnoDeTrabalho() {
+		return turnoDeTrabalho;
+	}
+
+	public void setTurnoDeTrabalho(String turnoDeTrabalho) {
+		this.turnoDeTrabalho = turnoDeTrabalho;
+	}
 	
 }
--- src/GerenciamentoHotel/Main.java
+++ src/GerenciamentoHotel/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		ArrayList<Hospede> listaHospedes = new ArrayList();
+		ArrayList<Funcionario> listaFuncionarios = new ArrayList();
 		Scanner sc = new Scanner(System.in);
 		int op1, op2;
 		
@@ -111,7 +112,89 @@ public class Main {
 					//
 				break;
 				case 4:
-					//
+					// Funcionarios
+					
+					System.out.println("\n1. Cadastrar novo Funcionario");
+					System.out.println("2. Editar Funcionario");
+					System.out.println("3. Cadastrar horas");
+					System.out.println("4. Calcular salario");
+					System.out.println("5. Voltar\n");
+					
+					System.out.print("Digite a opcao desejada: ");
+					op2 = sc.nextInt();
+					
+					switch (op2) {
+					case 1:
+						// Cadastrar Funcionario
+						sc.nextLine();
+						System.out.print("\nDigite o nome do funcionario: ");
+						String nome = sc.nextLine();
+						System.out.print("\nDigite o CPF do funcionario: ");
+						String cpf = sc.nextLine();
+						System.out.print("\nDigite o cargo do funcionario: ");
+						String cargo = sc.nextLine();
+						System.out.print("\nDigite o turno de trabalho: ");
+						String turnoDeTrabalho = sc.nextLine();
+						
+						Funcionario funcionario = new Funcionario(nome, cpf, cargo, turnoDeTrabalho);
+						
+						listaFuncionarios.add(funcionario);
+						
+						System.out.println(listaFuncionarios.get(0));
+						
+						System.out.println("\nFuncioario " + funcionario.getNome() + ", cadastrado com Sucesso!\n");
+					break;
+					case 2:
+						Boolean achou = false;
+						
+						sc.nextLine();
+						System.out.print("\nDigite o CPF do hospede que voce deseja editar: ");
+						String cpfEditar = sc.nextLine();
+						
+						// Encontrar Hospede
+						for (Hospede h : listaHospedes) {								
+							if (h.getCpf().equalsIgnoreCase(cpfEditar)) { // Encontrou
+								achou = true;
+								
+								System.out.println("Hospede " + h.getNome() + ", encontrado com sucesso!");
+								
+								System.out.print("\nDigite o nome corrigido do hospede: ");
+								String nomeNovo = sc.nextLine();
+								
+								h.setNome(nomeNovo);
+
+								System.out.print("\nDigite a data de nascimento corrigida do hospede: ");
+								String dataNascNova = sc.nextLine();
+								
+								Date dataNova = new Date();
+								h.setDataNasc(dataNova);
+								
+								System.out.print("\nDigite o novo endereco do hospede: ");
+								String enderecoNovo = sc.nextLine();
+								h.setEndereco(enderecoNovo);
+								
+								System.out.print("\nDigite o telefone de contato corrigido do hospede: ");
+								String telefoneNovo = sc.nextLine();
+								h.setNumeroContato(telefoneNovo);
+								
+								System.out.println("\nHospede Editado com Sucesso!\n");
+								break;
+							}
+						}
+						if (!achou) {
+							System.out.println("\nCPF nao encontrado...\n");								
+						}
+					break;
+					case 3:
+						// Cadastrar horas
+					break;
+					case 4:
+						// Calcular Salario
+					break;
+					case 5:
+						// Voltar
+					break;
+				}
 				break;
 				case 5:
 					//
